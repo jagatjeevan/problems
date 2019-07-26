81e747493156fa7e5e8556599e3f9ad05ff12ca5
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2019-07-26 08:55:36 +0530

    Jumping clouds

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 | Day | Total Problems |
 | --- | -------------- |
-| 9   | 6              |
+| 10  | 7              |
 
 ## Table of Contents
 
@@ -14,3 +14,4 @@
 | Jul 12 2019 | Finding classes in DOM      | [Find classes in DOM](./find-class.js)   |
 | Jul 21 2019 | Sock Merchant               | [Sock merchant](./sock-merchant.js)      |
 | Jul 24 2019 | Code comparison             | [Code comparison](./code-comparison.js)  |
+| Jul 26 2019 | Jumping clouds              | [Jumping clouds](./jumping-clouds.js)    |
---
+++ jumping-clouds.js
@@ -0,0 +1,34 @@
+/** Taken from hackerrank 
+ * 
+ * Emma is playing a new mobile game that starts with consecutively numbered clouds. 
+ * Some of the clouds are thunderheads and others are cumulus. 
+ * She can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus 1 or 2. 
+ * She must avoid the thunderheads. Determine the minimum number of jumps it will take Emma to jump from her starting postion to the last cloud. 
+ * It is always possible to win the game.
+ * 
+ * For each game, Emma will get an array of clouds numbered 0 if they are safe or 1 if they must be avoided. 
+ * For example, c=[0, 1, 0, 0, 0, 1, 0] indexed from 0 to 6. The number on each cloud is its index in the list so she must avoid the clouds at indexes 1 and 5. 
+ * She could follow the following two paths: 0-> 2-> 4-> 6 or 0-> 2-> 3-> 4-> 6. 
+ * The first path takes 3 jumps while the second takes 4.
+ * 
+ * So we need to find the minimum jumps
+ */
+
+ const jumpingClouds = (arr) => {
+     let path = [];
+     arr.forEach((item, index) => {
+         if(!item) {
+            path.push(index);
+         }
+         if(path.length > 2) {
+             let lastElem = path[path.length - 1];
+             let lastButOne = path[path.length - 2];
+             let lastButTwo = path[path.length - 3];
+             if((lastButTwo === lastButOne - 1) && (lastButOne === lastElem - 1)) {
+                 path = [...path.slice(0, path.length-2), lastElem];
+             }
+         }
+         
+     });
+     return path;
+ }
