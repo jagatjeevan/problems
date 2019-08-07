2dffc713250c8d0fd973446520bb9d5baa614da1
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2019-08-07 08:13:26 +0530

    vowels

---
+++ Regexp/start-end-with-same-vowel.js
@@ -0,0 +1,19 @@
+/**
+ * Regular Expressions
+ * Test the match that starts and ends with the same vowels
+ * Input: adklksjdflja
+ * Output: true
+ * 
+ * Input: adklksjdflje
+ * Output: false
+ * 
+ * Input: balksjdflkjasdf
+ * Output: false
+ * 
+ * */
+
+
+var vowelTest = str => {
+    const regex = /^(a|e|i|o|u).(a|e|i|o|u)$/;
+    return regex.test(str);
+}
