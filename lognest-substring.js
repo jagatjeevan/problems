0f8103a1f8190fa0ac8ddc8058157ada39e27458
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2019-09-25 10:30:16 +0530

    Find logest string complete

--- lognest-substring.js
+++ lognest-substring.js
@@ -1,31 +1,45 @@
 /**
- * 
+ *
  * Given a string, find the length of the longest substring without repeating characters.
- * 
+ *
  * Input: "abcabcbb"
- * Output: 3 
- * Explanation: The answer is "abc", with the length of 3. 
- * 
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ *
  * Input: "bbbbb"
  * Output: 1
  * Explanation: The answer is "b", with the length of 1.
- * 
+ *
  * Input: "pwwkew"
  * Output: 3
- * Explanation: The answer is "wke", with the length of 3. 
+ * Explanation: The answer is "wke", with the length of 3.
  * Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
- * 
+ *
  * */
 
-const findLargestSubstring = (str) => {
-    const subStrArr = [];
-    const tmp = [];
-    for(let i = 0; i < str.length; i++) {
-        let char = str.charAt(i);
-        tmp.forEach((item, index) => {
-            if(item.includes(char)) {
-
-            }
-        })
+var findLargestSubstring = input => {
+  var chars = input.split("");
+  var curr_char;
+  var str = "";
+  var longest_string = "";
+  var hash = {};
+  for (var i = 0; i < chars.length; i++) {
+    curr_char = chars[i];
+    if (!hash[chars[i]]) {
+      str += curr_char;
+      hash[chars[i]] = { index: i };
+    } else {
+      if (longest_string.length <= str.length) {
+        longest_string = str;
+      }
+      var prev_dupeIndex = hash[curr_char].index;
+      var str_FromPrevDupe = input.substring(prev_dupeIndex + 1, i);
+      str = str_FromPrevDupe + curr_char;
+      hash = {};
+      for (var j = prev_dupeIndex + 1; j <= i; j++) {
+        hash[input.charAt(j)] = { index: j };
+      }
     }
-}
+  }
+  return longest_string.length > str.length ? longest_string : str;
+};
