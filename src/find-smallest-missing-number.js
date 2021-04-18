6c8b7f444222f5a714ab95293fb7988e4c7e21fd
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2021-04-18 18:18:09 +0530

    Find the smallest missing positive number from the given array

---
+++ src/find-smallest-missing-number.js
@@ -0,0 +1,32 @@
+/**
+ * Find the smallest missing number in the array which is greater than 0
+ * Input : A = [1, 5, 7, 8],
+ * Output : 2
+ * Input : A = [-1, -5, -7, -8],
+ * Output : 1
+ *
+ * */
+
+const getPositiveArray = (arr) => {
+  return arr.filter((item) => item > 0);
+};
+
+var smallestMissingNumber = (A) => {
+  const sortedArray = getPositiveArray(A).sort((a, b) => a - b);
+  
+  if (sortedArray.length < 1) return 1;
+
+  let missingNumber = null;
+  sortedArray.find((currentValue, index, arr) => {
+    if (currentValue !== index + arr[0]) {
+      missingNumber = index + arr[0];
+      return true;
+    }
+  });
+
+  return missingNumber === null
+    ? sortedArray[sortedArray.length - 1] + 1
+    : missingNumber;
+};
+
+module.exports = smallestMissingNumber;
---
+++ test/find-smallest-missing-number.test.js
@@ -0,0 +1,11 @@
+const smallestMissingNumber = require("../src/find-smallest-missing-number");
+
+describe("find the smallest missing positive number in a given array", () => {
+  const input1 = [-1, -50, -8, 9, 11];
+  const input2 = [0, 1, 2, 3, 4, 5];
+  it("finds the smallest missing number", () => {
+    expect(smallestMissingNumber(input1)).toEqual(10);
+    expect(smallestMissingNumber(input2)).toEqual(6);
+    expect(smallestMissingNumber([-2, -10])).toEqual(1);
+  });
+});
