e0b9fc98729cb5dee6986b4af417bc86b23a3999
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2021-04-25 07:17:28 +0530

    Update time complexity for stock values

--- src/max-stock-profit.js
+++ src/max-stock-profit.js
@@ -9,19 +9,34 @@
  * */
 
 var maxStockProfit = (stockValues) => {
-  let bigProfit = 0;
+  if (stockValues.length < 2) return 0;
+
+  let minPrice = stockValues[0];
+  let maxProfit = stockValues[1] - stockValues[0];
+  for (let i = 1; i < stockValues.length; i++) {
+    let currentPrice = stockValues[i];
+    let potentialProfit = currentPrice - minPrice;
+    maxProfit = Math.max(maxProfit, potentialProfit);
+    minPrice = Math.min(currentPrice, minPrice);
+  }
+  return maxProfit;
+};
+
+// This has a time complexity of O(n)^2
+var maxStockProfit1 = (stockValues) => {
+  let maxProfit = 0;
   for (let i = 0; i <= stockValues.length; i++) {
     for (let j = i + 1; j < stockValues.length; j++) {
       if (stockValues[j] > stockValues[i]) {
-        bigProfit =
-          bigProfit < stockValues[j] - stockValues[i]
+        maxProfit =
+          maxProfit < stockValues[j] - stockValues[i]
             ? stockValues[j] - stockValues[i]
-            : bigProfit;
+            : maxProfit;
       }
     }
   }
 
-  return bigProfit;
+  return maxProfit;
 };
 
 module.exports = maxStockProfit;
--- test/max-stock-profit.test.js
+++ test/max-stock-profit.test.js
@@ -4,5 +4,7 @@ describe("max stock profit", () => {
   it("gives the maximum profit from the array", () => {
     expect(maxStockProfit([1, 5, 7, 8, 12, 76])).toEqual(75);
     expect(maxStockProfit([76, 12, 1, 5, 7, 8, 12])).toEqual(11);
+    expect(maxStockProfit([76, 86, 1, 5])).toEqual(10);
+    expect(maxStockProfit([76])).toEqual(0);
   });
 });
