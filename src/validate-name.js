843a31568cc9e92aa51b4c8239134a1128411dcf
Jagat Jeevan Sahoo <kanhajeevan@gmail.com>
2021-09-25 13:51:53 +0530

    Update validate name logic

--- src/validate-name.js
+++ src/validate-name.js
@@ -4,88 +4,70 @@
  * Criterias :
  * 1. Max length of the name string should be max of 75
  * 2. Each of the name should be less than or equal to 25
- * Assumption : You would have atleast 3 words of 1 char each
+ * Assumption : You would have atleast 3 words of 1 char each.
+ * Distribution of words is as per your choice
  *
  * */
 
-const maxCharAllowedInName = 75;
-const maxCharAllowedInNameSegment = 25;
+const maxCharInName = 75;
+const maxCharInNameSeg = 25;
 
-function getName(nameArr) {
-  const arr = [];
-  for (let i = 3; i > 0; i--) {
-    // 3 is for the length of "first, middle and last"
-    const nameSegment = Math.floor(nameArr.length / i);
-    arr.push(nameArr.splice(0, nameSegment));
-  }
-  return arr.map((item) => item.join(" "));
-}
-
-function checkNameSegment(nameSegment) {
-  const noOfWords = nameSegment.split(" ");
-  if (nameSegment.length <= maxCharAllowedInNameSegment) {
-    return ["", nameSegment, true];
-  }
-
-  if (noOfWords < 2) {
-    return ["", nameSegment, false];
-  }
-
-  let name = "";
-  let isValid = null;
+function getNameSegment(str, index) {
+  const wordCount = str.split(" ");
   let extra = "";
-  let wordsInSegment = nameSegment.split(" ");
-  for (let i = wordsInSegment.length; i > 1; i--) {
-    const tmp = wordsInSegment[i];
-    if ((name + tmp).length > maxCharAllowedInNameSegment) {
-      extra += tmp;
+  let nameSeg = "";
+  for (let i = 0; i <= wordCount.length - index; i++) {
+    if (parseInt((nameSeg + wordCount[i]).length, 10) < maxCharInNameSeg - 1) {
+      nameSeg = nameSeg === "" ? wordCount[i] : `${nameSeg} ${wordCount[i]}`;
     } else {
-      name += tmp;
+      extra = extra === "" ? wordCount[i] : `${extra} ${wordCount[i]}`;
     }
   }
 
-  isValid = name < maxCharAllowedInNameSegment;
+  const startIndex = wordCount.length - index + 1;
+  for (let i = startIndex; i < wordCount.length; i++) {
+    extra = extra === "" ? wordCount[i] : `${extra} ${wordCount[i]}`;
+  }
 
-  return [extra, name, isValid];
-}
-
-function getNameValidated(first, middle, last) {
-  const [lextra, lnameSegment, lisValid] = checkNameSegment(last);
-  if (!lisValid) return [first, middle, last, false];
-
-  const [mextra, mnameSegment, misValid] = checkNameSegment(
-    `${middle} ${lextra}`
-  );
-  if (!misValid) return [first, middle, last, false];
-
-  const [fextra, fnameSegment, fisValid] = checkNameSegment(
-    `${first} ${mextra}`
-  );
-  if (!fisValid || fextra !== "") return [first, middle, last, false];
-
-  return [fnameSegment, mnameSegment, lnameSegment, true];
+  const isValid = nameSeg.length < maxCharInNameSeg;
+  return [nameSeg, extra, isValid];
 }
 
 function validateName(str) {
   if (!str.length) return "No valid name";
-  if (str.length > maxCharAllowedInName)
-    return `Name should not be more than ${maxCharAllowedInName} characters`;
+  if (str.length > maxCharInName)
+    return `Name should not be more than ${maxCharInName} characters`;
 
-  const words = str.split(" ");
-  const [tmpfirst, tmpmiddle, tmplast] = getName(words);
-  const [first, middle, last, isValid] = getNameValidated(
-    tmpfirst,
-    tmpmiddle,
-    tmplast
-  );
+  const noOfWords = str.split(" ");
+  let first = "";
+  let middle = "";
+  let last = "";
+  let isValid;
+  switch (noOfWords.length) {
+    case 1:
+      last = str;
+      isValid = str.length < maxCharInNameSeg;
+      break;
 
-  if (!isValid) return "No valid name.";
+    case 2:
+      first = noOfWords[0];
+      last = noOfWords[1];
+      isValid =
+        noOfWords[0].length < maxCharInNameSeg &&
+        noOfWords[1].length < maxCharInNameSeg;
+      break;
 
-  return {
-    first: first.trim(),
-    middle: middle.trim(),
-    last: last.trim(),
-  };
+    default:
+      let extra, fIsValid, mIsValid, lIsValid;
+      [first, extra, fIsValid] = getNameSegment(str, 3);
+      [middle, extra, mIsValid] = getNameSegment(extra, 2);
+      [last, extra, lIsValid] = getNameSegment(extra, 1);
+      isValid = fIsValid && mIsValid && lIsValid && extra === "";
+  }
+
+  if (!isValid) return "No valid name";
+
+  return { first, middle, last };
 }
 
 module.exports = validateName;
--- test/validate-name.test.js
+++ test/validate-name.test.js
@@ -1,46 +1,91 @@
 const validateName = require("../src/validate-name");
 
-describe("Validate name", () => {
-  describe("handles negetive scenarios", () => {
-    test("empty string", () => {
-      expect(validateName("")).toBe("No valid name");
-    });
+test("empty string", () => {
+  expect(validateName("")).toBe("No valid name");
+});
 
-    test("name greater than 75 characters", () => {
-      expect(
-        validateName(
-          "1234567890123456789012345678901234567890123456789012345678901234567890123459"
-        )
-      ).toBe("Name should not be more than 75 characters");
+test("name greater than 75 characters", () => {
+  expect(
+    validateName(
+      "1234567890123456789012345678901234567890123456789012345678901234567890123459"
+    )
+  ).toBe("Name should not be more than 75 characters");
+});
+
+describe("For single word", () => {
+  test("less than 25", () => {
+    expect(validateName("Jagat")).toEqual({
+      first: "",
+      middle: "",
+      last: "Jagat",
+    });
+  });
+  test("for more than 25", () => {
+    expect(validateName("Jkajsdflkjasdljfalksjdflkjasdlfjalksjdfkj")).toEqual(
+      "No valid name"
+    );
+  });
+});
+
+describe("two words", () => {
+  test("less than 25", () => {
+    expect(validateName("Jagat Jeevan")).toEqual({
+      first: "Jagat",
+      middle: "",
+      last: "Jeevan",
     });
   });
 
-  describe("handles positive scenarios", () => {
-    test("happy scenario : 3 words less than 25 chars each", () => {
-      var name1 = "Jagat Jeevan Sahoo";
-      expect(validateName(name1)).toEqual({
-        first: "Jagat",
-        middle: "Jeevan",
-        last: "Sahoo",
-      });
-    });
+  test("for more than 25", () => {
+    expect(
+      validateName("Jkajsdflkjasdljfalksjdflkjasdlfjalksjdfkj sdfsdf")
+    ).toEqual("No valid name");
+    expect(
+      validateName("sdfsdf Jkajsdflkjasdljfalksjdflkjasdlfjalksjdfkj")
+    ).toEqual("No valid name");
+  });
+});
 
-    test("happy scenario : 4 words less than 25 chars each", () => {
-      var name2 = "Kanha Jagat Jeevan Sahoo";
-      expect(validateName(name2)).toEqual({
-        first: "Kanha",
-        middle: "Jagat",
-        last: "Jeevan Sahoo",
-      });
+describe("handles positive scenarios", () => {
+  test("happy scenario : 3 words less than 25 chars each", () => {
+    var name1 = "Jagat Jeevan Sahoo";
+    expect(validateName(name1)).toEqual({
+      first: "Jagat",
+      middle: "Jeevan",
+      last: "Sahoo",
     });
+  });
 
-    test("happy scenario : 5 words less than 25 chars each", () => {
-      var name2 = "Kanha Jagat Raghupati Jeevan Sahoo";
-      expect(validateName(name2)).toEqual({
-        first: "Kanha",
-        middle: "Jagat Raghupati",
-        last: "Jeevan Sahoo",
-      });
+  test("happy scenario : 4 words less than 25 chars each", () => {
+    var name2 = "Kanha Jagat Jeevan Sahoo";
+    expect(validateName(name2)).toEqual({
+      first: "Kanha Jagat",
+      middle: "Jeevan",
+      last: "Sahoo",
     });
+  });
+
+  test("happy scenario : 5 words less than 25 chars each", () => {
+    var name2 = "Kanha Jagat Raghupati Jeevan Sahoo";
+    expect(validateName(name2)).toEqual({
+      first: "Kanha Jagat Raghupati",
+      middle: "Jeevan",
+      last: "Sahoo",
+    });
+  });
+
+  test("happy scenario : more words less than 25 chars each", () => {
+    var name2 = "MANDAVA VEERA VENKATESHWARA CHALLAPATHIRAO AAIYER IYER";
+    expect(validateName(name2)).toEqual({
+      first: "MANDAVA VEERA",
+      middle: "VENKATESHWARA AAIYER",
+      last: "CHALLAPATHIRAO IYER",
+    });
+  });
+
+  test("More words with one more than 25 chars", () => {
+    var name2 =
+      "MANDAVA VEERA VENKATESHWARA VENKATESHWARACHALLAPATHI AAIYER IYER";
+    expect(validateName(name2)).toEqual("No valid name");
   });
 });
